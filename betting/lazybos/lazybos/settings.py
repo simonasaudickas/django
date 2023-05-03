42ef16dbf53f22b2693ba6f5a5ab321b93614989
Simonas Audickas <simonas.audickas@gmail.com>
2023-05-03 16:07:58 +0300

    naujas kurimas

--- betting/lazybos/lazybos/settings.py
+++ betting/lazybos/lazybos/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bet',
 ]
 
 MIDDLEWARE = [
@@ -72,11 +73,19 @@ WSGI_APPLICATION = 'lazybos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'puslapis',
+        'PASSWORD': 'blabla',
     }
 }
 
